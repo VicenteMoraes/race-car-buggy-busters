4e5ec2a8ab86af7e6ed7680f05ddb3fad893d00c
ossmos <flashthemusic@gmx.de>
2024-11-15 17:43:38 +0100

    Add ros_gz_bridge for Twist msgs

--- race_car_ws/gazebo.launch.py
+++ race_car_ws/gazebo.launch.py
@@ -8,11 +8,18 @@ def generate_launch_description():
                 namespace="f110",
                 executable="wasd_control_node",
                 name="wasd_control"
-                ),
+            ),
             Node(
                 package="gazebo_f110",
                 namespace="gazebo",
                 executable="ackermann_to_twist",
                 name="ackermann_to_twist"
-                )
+            ),
+            Node(
+                package='ros_gz_bridge',
+                executable='parameter_bridge',
+                arguments=["/cmd_vel@geometry_msgs/msg/Twist@ignition.msgs.Twist",],
+                remappings=[],
+                output='screen'
+            ),
         ])
