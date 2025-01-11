1691fea1695da4dc95cf1b6db8129334c1872eb5
Ahmad-sh7 <ahmadalshihabi99@gmail.com>
2025-01-11 13:53:29 +0100

    fix transforming position

--- race_car_ws/src/gazebo/gazebo_f110/gazebo_f110/pose_transformer.py
+++ race_car_ws/src/gazebo/gazebo_f110/gazebo_f110/pose_transformer.py
@@ -1,6 +1,6 @@
 import rclpy
 from rclpy.node import Node
-from geometry_msgs.msg import PoseArray, PoseStamped
+from geometry_msgs.msg import PoseArray, PoseStamped, Pose
 from gazebo_f110.transformer.transform_utils import transform_pose_gazebo_to_ros2
 
 class GazeboPoseTransformer(Node):
@@ -18,9 +18,9 @@ class GazeboPoseTransformer(Node):
             10
         )
 
-    def pose_callback(self, msg_in):
-        msg_pos_in = msg_in.pose[0]
-        msg_quat_in = msg_in.pose[1]
+    def pose_callback(self, msg_in: PoseArray):
+        msg_pos_in = msg_in.poses[0]
+        msg_quat_in = msg_in.poses[1]
         pos_g = (msg_pos_in.position.x, msg_pos_in.position.y, msg_pos_in.position.z)
         quat_g = (msg_quat_in.orientation.x, msg_quat_in.orientation.y,
                   msg_quat_in.orientation.z, msg_quat_in.orientation.w)
@@ -28,13 +28,15 @@ class GazeboPoseTransformer(Node):
         pos_r, quat_r = transform_pose_gazebo_to_ros2(pos_g, quat_g)
 
         msg_out = PoseArray()
-        msg_out.pose[0].position.x = pos_r[0]
-        msg_out.pose[0].position.y = pos_r[1]
-        msg_out.pose[0].position.z = pos_r[2]
-        msg_out.pose[1].orientation.x = quat_r[0]
-        msg_out.pose[1].orientation.y = quat_r[1]
-        msg_out.pose[1].orientation.z = quat_r[2]
-        msg_out.pose[1].orientation.w = quat_r[3]
+        msg_out.poses.append(Pose())
+        msg_out_pose = msg_out.poses[0]
+        msg_out_pose.position.x = pos_r[0]
+        msg_out_pose.position.y = pos_r[1]
+        msg_out_pose.position.z = pos_r[2]
+        msg_out_pose.orientation.x = quat_r[0]
+        msg_out_pose.orientation.y = quat_r[1]
+        msg_out_pose.orientation.z = quat_r[2]
+        msg_out_pose.orientation.w = quat_r[3]
 
         self.pub_pose.publish(msg_out)
 
