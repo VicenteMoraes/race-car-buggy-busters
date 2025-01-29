d8ecd08cee786fc4d4ba3da7b710613f3f40f13c
Kevin Losing <kevin.losing@protonmail.com>
2025-01-29 20:22:19 +0100

    Restructure model/world & add color to cones

--- race_car_ws/src/gazebo/gazebo_f110/setup.py
+++ race_car_ws/src/gazebo/gazebo_f110/setup.py
@@ -4,20 +4,31 @@ from glob import glob
 
 package_name = 'gazebo_f110'
 
+def collect_model_files(model_dir='model'):
+    data_files = []
+    for root, dirs, files in os.walk(model_dir):
+        for file in files:
+            file_path = os.path.join(root, file)
+            rel_path = os.path.relpath(file_path, model_dir)
+            target_dir = os.path.join('share', package_name, 'model', os.path.dirname(rel_path))
+            data_files.append((target_dir, [file_path]))
+    return data_files
+
+data_files = [
+    ('share/ament_index/resource_index/packages',
+        ['resource/' + package_name]),
+    ('share/' + package_name, ['package.xml']),
+    (os.path.join('share', package_name), glob('launch/*.launch.py')),
+    (os.path.join('share', package_name, "world"), glob('world/*.sdf')),
+]
+
+data_files += collect_model_files('model')
+
 setup(
     name=package_name,
     version='0.0.1',
     packages=find_packages(exclude=['test']),
-    data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name), glob('launch/*.launch.py')),
-        (os.path.join('share', package_name, "world/"), glob('world/*.sdf')),
-        (os.path.join('share', package_name, "model/"), glob('model/*.sdf')),
-        (os.path.join('share', package_name, "model/"), glob('model/*.dae')),
-        (os.path.join('share', package_name, "model/"), glob('model/*.config')),
-    ],
+    data_files=data_files,
     install_requires=['setuptools'],
     zip_safe=True,
     entry_points={
