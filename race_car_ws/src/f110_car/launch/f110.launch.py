ba7fd18276c92b599c67fb8da908bacaa406bff1
Kevin Losing <kevin.losing@protonmail.com>
2025-02-18 19:04:00 +0100

    Use sim time + add launch file to setup for testing

--- race_car_ws/src/f110_car/launch/f110.launch.py
+++ race_car_ws/src/f110_car/launch/f110.launch.py
@@ -6,36 +6,42 @@ def generate_launch_description():
         package="f110_car",
         executable="move_to_point",
         name="move_to_point",
+        parameters=[{'use_sim_time': True}],
     )
 
     lidar_node = Node(
         package='test_package',
         executable='lidar_node',
         name='lidar_node',
+        parameters=[{'use_sim_time': True}],
     )
 
     yolo_node = Node(
         package='test_package',
         executable='yolo_node',
         name='yolo_node',
+        parameters=[{'use_sim_time': True}],
     )
 
     sensor_fusion_node = Node(
         package='test_package',
         executable='sensor_fusion_node',
         name='sensor_fusion_node',
+        parameters=[{'use_sim_time': True}],
     )
 
     mapping_node = Node(
         package='test_package',
         executable='mapping_node',
         name='mapping_node',
+        parameters=[{'use_sim_time': True}],
     )
 
     odom_tf_node = Node(
         package='test_package',
         executable='odom_tf_node',
         name='odom_tf_node',
+        parameters=[{'use_sim_time': True}],
     )
 
     static_tf_base_camera = Node(
@@ -47,6 +53,7 @@ def generate_launch_description():
             '0', '0', '0',
             'base_link', 'camera_link'
         ],
+        parameters=[{'use_sim_time': True}],
     )
 
     return LaunchDescription([
